59a998083e78209c3bdd8e6c8f42bfa8a2014244
smoroa14 <robertscho1240@gmail.com>
2019-02-04 10:16:03 +0100

    added

--- src/game/beans/Springer.java
+++ src/game/beans/Springer.java
@@ -29,56 +29,56 @@ public final class Springer extends Piece {
         // Rechts unten 1 ++
         if (pos.x + 1 <= 7 && pos.y + 2 <= 7) {
             if (steht_auf.getBoard().getSquare(pos.x + 1, pos.y + 2).getBesetzt_von() == null
-                    || steht_auf.getBoard().getSquare(pos.x + 1, pos.y + 2).getBesetzt_von().getColor().equals(getColor())) {
+                    || !steht_auf.getBoard().getSquare(pos.x + 1, pos.y + 2).getBesetzt_von().getColor().equals(getColor())) {
                 moves.add(new Point(pos.x + 1, pos.y + 2));
             }
         }
         // Rechts unten 2 ++
         if (pos.x + 2 <= 7 && pos.y + 1 <= 7) {
             if (steht_auf.getBoard().getSquare(pos.x + 2, pos.y + 1).getBesetzt_von() == null
-                    || steht_auf.getBoard().getSquare(pos.x + 2, pos.y + 1).getBesetzt_von().getColor().equals(getColor())) {
+                    || !steht_auf.getBoard().getSquare(pos.x + 2, pos.y + 1).getBesetzt_von().getColor().equals(getColor())) {
                 moves.add(new Point(pos.x + 2, pos.y + 1));
             }
         }
         // Links unten 1 -+
         if (pos.x - 1 >= 0 && pos.y + 2 <= 7) {
             if (steht_auf.getBoard().getSquare(pos.x - 1, pos.y + 2).getBesetzt_von() == null
-                    || steht_auf.getBoard().getSquare(pos.x - 1, pos.y + 2).getBesetzt_von().getColor().equals(getColor())) {
+                    || !steht_auf.getBoard().getSquare(pos.x - 1, pos.y + 2).getBesetzt_von().getColor().equals(getColor())) {
                 moves.add(new Point(pos.x - 1, pos.y + 2));
             }
         }
         //Links unten 2 -+
         if (pos.x - 2 >= 0 && pos.y + 1 <= 7) {
             if (steht_auf.getBoard().getSquare(pos.x - 2, pos.y + 1).getBesetzt_von() == null
-                    || steht_auf.getBoard().getSquare(pos.x - 2, pos.y + 1).getBesetzt_von().getColor().equals(getColor())) {
+                    || !steht_auf.getBoard().getSquare(pos.x - 2, pos.y + 1).getBesetzt_von().getColor().equals(getColor())) {
                 moves.add(new Point(pos.x - 2, pos.y + 1));
             }
         }
         //Rechts oben 1 +-
         if (pos.x + 1 <= 7 && pos.y - 2 >= 0) {
             if (steht_auf.getBoard().getSquare(pos.x + 1, pos.y - 2).getBesetzt_von() == null
-                    || steht_auf.getBoard().getSquare(pos.x + 1, pos.y - 2).getBesetzt_von().getColor().equals(getColor())) {
+                    || !steht_auf.getBoard().getSquare(pos.x + 1, pos.y - 2).getBesetzt_von().getColor().equals(getColor())) {
                 moves.add(new Point(pos.x + 1, pos.y - 2));
             }
         }
         // Rechts Oben 2 +-
         if (pos.x + 2 <= 7 && pos.y - 1 >= 0) {
             if (steht_auf.getBoard().getSquare(pos.x + 2, pos.y - 1).getBesetzt_von() == null
-                    || steht_auf.getBoard().getSquare(pos.x + 2, pos.y - 1).getBesetzt_von().getColor().equals(getColor())) {
+                    || !steht_auf.getBoard().getSquare(pos.x + 2, pos.y - 1).getBesetzt_von().getColor().equals(getColor())) {
                 moves.add(new Point(pos.x + 2, pos.y - 1));
             }
         }
         //Links oben 1 --
         if (pos.x - 1 >= 0 && pos.y - 2 >= 0) {
             if (steht_auf.getBoard().getSquare(pos.x - 1, pos.y - 2).getBesetzt_von() == null
-                    || steht_auf.getBoard().getSquare(pos.x - 1, pos.y - 2).getBesetzt_von().getColor().equals(getColor())) {
+                    || !steht_auf.getBoard().getSquare(pos.x - 1, pos.y - 2).getBesetzt_von().getColor().equals(getColor())) {
                 moves.add(new Point(pos.x - 1, pos.y - 2));
             }
         }
         //Links oben 2 --
         if (pos.x - 2 >= 0 && pos.y - 1 >= 0) {
             if (steht_auf.getBoard().getSquare(pos.x - 2, pos.y - 1).getBesetzt_von() == null
-                    || steht_auf.getBoard().getSquare(pos.x - 2, pos.y - 1).getBesetzt_von().getColor().equals(getColor())) {
+                    || !steht_auf.getBoard().getSquare(pos.x - 2, pos.y - 1).getBesetzt_von().getColor().equals(getColor())) {
                 moves.add(new Point(pos.x - 2, pos.y - 1));
             }
         }
