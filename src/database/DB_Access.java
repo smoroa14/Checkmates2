67133833ff099375e5fc303fb8758952e6a286b8
greifadler <fabiangreimel@gmail.com>
2019-04-09 08:13:37 +0200

    Bugfixes

--- src/database/DB_Access.java
+++ src/database/DB_Access.java
@@ -76,7 +76,7 @@ public class DB_Access {
     public Player getUser(String username, String password) {
         TypedQuery<Player> user = em.createNamedQuery("Player.getPlayer", Player.class);
         user.setParameter("username", username);
-        user.setParameter("password", getHashOfString(password));
+        user.setParameter("password", ""+getHashOfString(password));
         List<Player> userList = user.getResultList();
         if (userList != null) {
             return userList.get(0);
